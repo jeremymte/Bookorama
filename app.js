3136e75612aad821bc893cef85a232a846ee882f
Jeremy Te <jayte718@gmail.com>
2019-09-05 21:02:15 -0400

    using querySelector

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const wmf = document.querySelector('#book-list li:nth-child(2) .name');
-console.log(wmf);
+console.log(wmf)
 
 var books = document.querySelectorAll('#book-list li .name');
 console.log(books);
 
 books = document.querySelectorAll('#book-list li .name');
+
+Array.from(books).forEach(function(book) {
+    console.log(book);
+})
