1997504d20729c072d90c586b7312851a0f57dd9
Jeremy Te <jayte718@gmail.com>
2019-09-10 17:58:42 -0400

    Added tab content to toggle between 'About' and 'Contact' information

--- app.js
+++ app.js
@@ -57,3 +57,19 @@ searchBar.addEventListener('keyup', function (e) {
         }
     })
 });
+
+//tabbed content
+var tabs = document.querySelector('.tabs');
+var panels = document.querySelectorAll('.panel');
+tabs.addEventListener('click', function (e) {
+    if (e.target.tagName == 'LI') {
+        const targetPanel = document.querySelector(e.target.dataset.target);
+        panels.forEach(function (panel) {
+            if (panel == targetPanel) {
+                panel.classList.add('active');
+            } else {
+                panel.classList.remove('active')
+            }
+        })
+    }
+})
