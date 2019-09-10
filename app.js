93b1c08c58a3c9ce254146228e995e82ab5e9690
Jeremy Te <jayte718@gmail.com>
2019-09-09 20:25:36 -0400

    Created a filter to search the book titles

--- app.js
+++ app.js
@@ -41,4 +41,19 @@ hideBox.addEventListener('change', function (e) {
     } else {
         list.style.display = "initial";
     }
-})
+});
+
+//filter books
+var searchBar = document.forms['search-books'].querySelector('input');
+searchBar.addEventListener('keyup', function (e) {
+    const term = e.target.value.toLowerCase();
+    const books = list.getElementsByTagName('li');
+    Array.from(books).forEach(function (book) {
+        const title = book.firstElementChild.textContent;
+        if (title.toLowerCase().indexOf(term) != -1) {
+            book.style.display = 'block';
+        } else {
+            book.style.display = 'none';
+        }
+    })
+});
